a330752c3f834579839908b8d991ee0c5b7ea2d9
dmitry-mak <akkaru@gmail.com>
2025-03-02 09:41:24 +0200

    test class refactoring

--- src/test/java/ru/netology/silenium/AppOrderWithSeleniumTest.java
+++ src/test/java/ru/netology/silenium/AppOrderWithSeleniumTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class AppOrderTestWithSelenium {
+public class AppOrderWithSeleniumTest {
 
     @Test
     public void seleniumTest() {
@@ -19,7 +19,7 @@ public class AppOrderTestWithSelenium {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
 
         chromeDriver = new ChromeDriver(options);
 
